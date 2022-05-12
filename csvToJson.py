5e71c41989e53b1118b5d866ab7c21e83fb164f9
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-05-12 21:19:23 +0900

    Feat: csv to json

--- csvToJson.py
+++ csvToJson.py
@@ -1,18 +1,31 @@
 import json
 import csv
 from collections import OrderedDict
+from dictionary import *
 
 samples = []
 
 with open('hci+d 스승의날 편지.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
-
+    next(reader)
+    next(reader)
 
     for row in reader:
         person = OrderedDict()
         person['name'] = row[1]
-        person['level'] = row[5]
+        if row[4] in pokemon_num:
+            person['pokenum'] = pokemon_num[row[4]]
+        else:
+            print("find not pokemon")
+            print(row[1], row[4])
+            person['pokenum'] = row[4]
+        try:
+            person['level'] = int(row[5])
+        except:
+            print("find not num")
+            print(row[1], row[5])
+            person['level'] = row[5]
         person['dept'] = row[2]
         person['status'] = row[3]
         person['letter-j'] = row[6]
